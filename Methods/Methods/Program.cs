52e208fb5b57dfd20def0d766e962f52aa669ff3
Shivbak <shivangibakrania@gmail.com>
2016-06-21 16:13:22 +0100

    Day2

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class car
+    {
+        public string make;
+        public string model;
+        public int price;
+        public Boolean sold;
+
+        public static int total_numbercars;
+
+        public void AddCar(string make,string model, int price)
+        {
+            this.make = make;
+            this.model = model;
+            this.price = price;
+        }
+
+        public void SoldCar(bool sold,int price)
+        {
+            total_numbercars--;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car car1 = new car();
+            car1.AddCar("BMW", "ABC", 10000);
+            car.total_numbercars = 10;
+            Console.WriteLine("make :{0} , total cars : {1}",car1.model,car.total_numbercars);
+            //car1.SoldCar()
+        }
+    }
+}
---
+++ classes/classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    class car
+    {
+        public string make;
+        public string model;
+        public int price;
+
+        public static int total_numberofcars;
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            car car1 = new car();
+            car1.make = "Reliant";
+            car1.model = "abc";
+            car1.price = 1000000000;
+
+            car car2 = new car();
+            car2.make = "BMW";
+            car2.model = "XYZ";
+            car2.price = 20000;
+
+            car.total_numberofcars = 6;
+
+            Console.WriteLine("Make {1} , model {0} , price {3:N0}, total cars {2}", car1.model,car1.make,car.total_numberofcars,car1.price);
+        }
+    }
+}
