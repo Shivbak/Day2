cefe0a8dabf7c4d9bd4c322f1158717a9a940d3c
Shivbak <shivangibakrania@gmail.com>
2016-06-22 16:32:37 +0100

    day2 done

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Vehicle
+    {
+        public static int TotalNoOfVehicle;
+
+       public static void Total_Vehicle()
+        {
+            Console.WriteLine("Total No of Vehivle in Showroom : {0}",TotalNoOfVehicle);
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public string makecar;
+        public string modelcar;
+        public int pricecar;
+        public Boolean soldcar;
+
+        public void Addcar(string make,string model, int price)
+        {
+            this.makecar = make;
+            this.modelcar = model;
+            this.pricecar = price;
+            TotalNoOfVehicle++;
+            Vehicle.Total_Vehicle();
+        }
+
+        public void Soldcar(bool sold,int price)
+        {
+            this.soldcar = sold;
+            this.pricecar = price;
+            TotalNoOfVehicle--;
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public string makebike;
+        public string modelbike;
+        public int pricebike;
+        public Boolean soldbike;
+
+        public void AddBike(string make, string model, int price)
+        {
+            this.makebike = make;
+            this.modelbike = model;
+            this.pricebike = price;
+            TotalNoOfVehicle++;
+        }
+
+        public void SoldBike(bool sold, int price)
+        {
+            this.soldbike = sold;
+            this.pricebike = price;
+            TotalNoOfVehicle--;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Addcar("BMW", "ABC", 10000);
+
+            Bike bike1 = new Bike();
+            bike1.AddBike("ABC","XYZ",20000);
+            Vehicle.Total_Vehicle();
+        }
+    }
+}
--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -12,6 +12,7 @@ namespace Methods
         public string model;
         public int price;
         public Boolean sold;
+        
 
         public static int total_numbercars;
 
@@ -24,18 +25,46 @@ namespace Methods
 
         public void SoldCar(bool sold,int price)
         {
+            //this.make = make;
+            this.sold = sold;
+            //this.price = price;
             total_numbercars--;
         }
+
+        public static  void Listofcars(List<car> listofcars)
+        {
+            Console.WriteLine("in the list cars method");
+
+           foreach(car item in listofcars)
+            {
+                Console.WriteLine(item.model + " "+ item.make + " " + item.price);
+            }
+           
+        } 
     }
     class Program
     {
         static void Main(string[] args)
         {
+            List<car> myCarList = new List<car>();
+
             car car1 = new car();
+            
             car1.AddCar("BMW", "ABC", 10000);
-            car.total_numbercars = 10;
-            Console.WriteLine("make :{0} , total cars : {1}",car1.model,car.total_numbercars);
-            //car1.SoldCar()
+            myCarList.Add(car1);
+            car.total_numbercars++;
+            Console.WriteLine("make :{0} , total cars : {1}",car1.model, car.total_numbercars);
+
+            car car2 = new car();
+            car2.AddCar("Audi", "R9", 100000);
+            
+            myCarList.Add(car2);
+            car.total_numbercars++;
+            //Console.WriteLine("sold : {0} , price : {1} , total cars: {2}",car2.sold,car2.price,car.total_numbercars);
+
+            car2.SoldCar(true, 30000);
+
+            car.Listofcars(myCarList);
         }
     }
 }
